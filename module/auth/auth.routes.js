117bff4e292c9dc39b4e6ee4b49dff6e72d41d1e
aminvv <aminviper1378@gmail.com>
2025-06-26 13:33:41 +0330

    check Otp code

--- module/auth/auth.routes.js
+++ module/auth/auth.routes.js
@@ -1,10 +1,11 @@
  const{Router}=require('express')
- const{sendOtp}=require('./auth.service')
+ const{sendOtp, checkOtp}=require('./auth.service')
 
 
 const router=Router()
 
 router.post('/send-otp',sendOtp)
+router.post('/check-otp',checkOtp)
 
 
 module.exports={
--- module/auth/auth.service.js
+++ module/auth/auth.service.js
@@ -1,44 +1,46 @@
+const createHttpError = require("http-errors");
+const { model } = require("../../config/sequelize.config");
 const { User, Otp } = require("../user/model/user.model")
 
 async function sendOtp(req, res, next) {
- try {
-    const { mobile } = req.body;
+    try {
+        const { mobile } = req.body;
 
-    const code = (Math.floor(10000 + Math.random() * 90000)).toString(); // ۵ رقمی و رشته
-    const expires_in = 2 * 60; // ۲ دقیقه
+        const code = (Math.floor(10000 + Math.random() * 90000)).toString()
+        const expires_in = 2 * 60;
 
-    let user = await User.findOne({ where: { mobile } });
-    let otp;
+        let user = await User.findOne({ where: { mobile } });
+        let otp;
 
-    if (!user) {
-      user = await User.create({ mobile });
+        if (!user) {
+            user = await User.create({ mobile });
 
-      otp = await Otp.create({
-        code,
-        expires_in,
-        userId: user.id
-      });
+            otp = await Otp.create({
+                code,
+                expires_in,
+                userId: user.id
+            });
 
-    } else {
-      otp = await Otp.findOne({ where: { userId: user.id } });
+        } else {
+            otp = await Otp.findOne({ where: { userId: user.id } });
 
-      if (otp) {
-        otp.code = code;
-        otp.expires_in = expires_in;
-        await otp.save();
-      } else {
-        otp = await Otp.create({
-          code,
-          expires_in,
-          userId: user.id
+            if (otp) {
+                otp.code = code;
+                otp.expires_in = expires_in;
+                await otp.save();
+            } else {
+                otp = await Otp.create({
+                    code,
+                    expires_in,
+                    userId: user.id
+                });
+            }
+        }
+
+        return res.json({
+            message: "send otp code successfully",
+            code
         });
-      }
-    }
-
-    return res.json({
-      message: "send otp code successfully",
-      code
-    });
 
 
 
@@ -49,7 +51,31 @@ async function sendOtp(req, res, next) {
 }
 
 
+async function checkOtp(req, res, next) {
+    try {
+
+        const { mobile, code } = req.body
+        console.log("'mobile'++++++>>>>" ,mobile);
+        console.log("'code'++++++>>>>" ,code);
+        const user = await User.findOne({
+            where: { mobile },
+            include: [
+                { model: Otp, as: "otp" }
+            ]
+        })
+
+        if (!user) throw createHttpError(401, 'not found  user account')
+        if(user?.otp?.code!==code) throw createHttpError(401 ,"otp code is invalid")    
+        if(user?.otp?.expires_in > new Date()) throw createHttpError(401 ," otp code  is expired")    
+             return res.json({
+            message:" logged in successfully "})
+    } catch (error) {
+        next(error)
+    }
+}
+
 
 module.exports = {
-    sendOtp
+    sendOtp,
+    checkOtp,
 }
