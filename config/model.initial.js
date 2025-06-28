48a20519e7bc8986b70f7045338fdd6508cd14ab
aminvv <aminviper1378@gmail.com>
2025-06-28 17:53:52 +0330

    creatte model and relation basket

--- config/model.initial.js
+++ config/model.initial.js
@@ -1,3 +1,4 @@
+const { Basket } = require("../module/basket/model/basket.model");
 const { Product, ProductDetail, ProductColor, ProductSize } = require("../module/product/model/product.model");
 const { Otp, User } = require("../module/user/model/user.model");
 const sequelize = require("./sequelize.config");
@@ -16,6 +17,17 @@ async function initDataBase() {
     User.hasOne(Otp,{foreignKey:"userId" ,sourceKey:"id" ,as:"otp"})
     Otp.belongsTo(User,{foreignKey:"userId" ,targetKey:"id" })
     
+
+    Product.hasMany(Basket,{foreignKey:"productId",sourceKey:"id" ,as:"basket"})
+    ProductColor.hasMany(Basket,{foreignKey:"colorId",sourceKey:"id" ,as:"basket"})
+    ProductSize.hasMany(Basket,{foreignKey:"sizeId",sourceKey:"id" ,as:"basket"})
+    User.hasMany(Basket,{foreignKey:"userId",sourceKey:"id" ,as:"basket"})
+
+
+    Basket.belongsTo(Product,{foreignKey:"productId", targetKey:"id",as:"product"})
+    Basket.belongsTo(User,{foreignKey:"userId", targetKey:"id",as:"user"})
+    Basket.belongsTo(ProductColor,{foreignKey:"colorId", targetKey:"id",as:"color"})
+    Basket.belongsTo(ProductSize,{foreignKey:"sizeId", targetKey:"id",as:"size"})
     await sequelize.sync({ alter: true })
 
 }
