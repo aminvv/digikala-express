242a6f255a909611b8b07dde981a4fea5a1849ad
aminvv <aminviper1378@gmail.com>
2025-07-02 14:15:55 +0330

    create  order model & order_items model & payment  model

--- config/model.initial.js
+++ config/model.initial.js
@@ -1,37 +1,48 @@
 const { Basket } = require("../module/basket/model/basket.model");
+const { Order, OrderItems } = require("../module/order/model/order.model");
 const { Discount } = require("../module/discount/model/discount.model");
 const { Product, ProductDetail, ProductColor, ProductSize } = require("../module/product/model/product.model");
 const { Otp, User } = require("../module/user/model/user.model");
 const sequelize = require("./sequelize.config");
+const { Payment } = require("../module/payment/model/payment.model");
 
 async function initDataBase() {
-    Product.hasMany(ProductDetail, { foreignKey: "productId", sourceKey: "id",  as: "details" });
+    Product.hasMany(ProductDetail, { foreignKey: "productId", sourceKey: "id", as: "details" });
     ProductDetail.belongsTo(Product, { foreignKey: "productId", targetKey: "id" });
 
-    Product.hasMany(ProductColor, { foreignKey: "productId", sourceKey: "id",  as: "colors" });
+    Product.hasMany(ProductColor, { foreignKey: "productId", sourceKey: "id", as: "colors" });
     ProductColor.belongsTo(Product, { foreignKey: "productId", targetKey: "id" });
 
-    Product.hasMany(ProductSize, { foreignKey: "productId", sourceKey: "id",  as: "sizes" });
+    Product.hasMany(ProductSize, { foreignKey: "productId", sourceKey: "id", as: "sizes" });
     ProductSize.belongsTo(Product, { foreignKey: "productId", targetKey: "id" });
 
 
-    User.hasOne(Otp,{foreignKey:"userId" ,sourceKey:"id" ,as:"otp"})
-    Otp.belongsTo(User,{foreignKey:"userId" ,targetKey:"id" })
-    
-
-    Product.hasMany(Basket,{foreignKey:"productId",sourceKey:"id" ,as:"basket"})
-    ProductColor.hasMany(Basket,{foreignKey:"colorId",sourceKey:"id" ,as:"basket"})
-    ProductSize.hasMany(Basket,{foreignKey:"sizeId",sourceKey:"id" ,as:"basket"})
-    User.hasMany(Basket,{foreignKey:"userId",sourceKey:"id" ,as:"basket"})
-    Discount.hasMany(Basket,{foreignKey:"discountId",sourceKey:"id" ,as:"basket"})
+    User.hasOne(Otp, { foreignKey: "userId", sourceKey: "id", as: "otp" })
+    Otp.belongsTo(User, { foreignKey: "userId", targetKey: "id" })
 
 
-    Basket.belongsTo(Product,{foreignKey:"productId", targetKey:"id",as:"product"})
-    Basket.belongsTo(User,{foreignKey:"userId", targetKey:"id",as:"user"})
-    Basket.belongsTo(ProductColor,{foreignKey:"colorId", targetKey:"id",as:"color"})
-    Basket.belongsTo(ProductSize,{foreignKey:"sizeId", targetKey:"id",as:"size"})
-    Basket.belongsTo(Discount,{foreignKey:"discountId", targetKey:"id",as:"discount"})
+    Product.hasMany(Basket, { foreignKey: "productId", sourceKey: "id", as: "basket" })
+    ProductColor.hasMany(Basket, { foreignKey: "colorId", sourceKey: "id", as: "basket" })
+    ProductSize.hasMany(Basket, { foreignKey: "sizeId", sourceKey: "id", as: "basket" })
+    User.hasMany(Basket, { foreignKey: "userId", sourceKey: "id", as: "basket" })
+    Discount.hasMany(Basket, { foreignKey: "discountId", sourceKey: "id", as: "basket" })
+
+
+    Basket.belongsTo(Product, { foreignKey: "productId", targetKey: "id", as: "product" })
+    Basket.belongsTo(User, { foreignKey: "userId", targetKey: "id", as: "user" })
+    Basket.belongsTo(ProductColor, { foreignKey: "colorId", targetKey: "id", as: "color" })
+    Basket.belongsTo(ProductSize, { foreignKey: "sizeId", targetKey: "id", as: "size" })
+    Basket.belongsTo(Discount, { foreignKey: "discountId", targetKey: "id", as: "discount" })
     await sequelize.sync({ alter: true })
+
+
+    Order.hasMany(OrderItems, { foreignKey: "orderId", sourceKey: "id", as: "items" });
+    User.hasMany(Order, { foreignKey: "userId", sourceKey: "id", as: "orders" });
+    OrderItems.belongsTo(Order, { foreignKey: "orderId", targetKey: "id" });
+
+    Order.hasOne(Payment, { foreignKey: { name: "orderId", onDelete: "CASCADE" }, sourceKey: "id", as: "payment" });
+    Payment.belongsTo(Order, { foreignKey: { name: "orderId", onDelete: "CASCADE" }, targetKey: "id", as: "order" });
+
 
 }
 
---
+++ module/order/model/order.model.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("@sequelize/core");
+const sequelize = require("../../../config/sequelize.config");
+const {OrderStatus} = require("../../../common/constant/order.constant");
+const Order = sequelize.define("order", {
+    id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    paymentId: {type: DataTypes.INTEGER, allowNull: true},
+    status: {type: DataTypes.ENUM(...Object.values(OrderStatus)), defaultValue: OrderStatus.Pending},
+    address: {type: DataTypes.TEXT},
+    userId: {type: DataTypes.INTEGER},
+    total_amount: {type: DataTypes.DECIMAL(4,2)},
+    final_amount: {type: DataTypes.DECIMAL(4,2)},
+    discount_amount: {type: DataTypes.DECIMAL(4,2)},
+    reason: {type: DataTypes.STRING, allowNull: true},
+}, {
+    modelName: "order",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: false
+});
+
+const OrderItems = sequelize.define("order_item", {
+    id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    orderId: {type: DataTypes.INTEGER},
+    productId: {type: DataTypes.INTEGER},
+    sizeId: {type: DataTypes.INTEGER, allowNull: true},
+    colorId: {type: DataTypes.INTEGER, allowNull: true},
+    count: {type: DataTypes.INTEGER},
+
+}, {timestamps: false, modelName: "order_item"});
+
+module.exports = {
+    Order,
+    OrderItems
+};
